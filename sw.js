aa84a95c54bb2c7f69424e6f4900ce3d2e04a305
Buddy <buddy@grokbot.local>
2026-09-22 22:00:48 -0400

    Fix Email: open in Safari, preserve thread, add Copy link

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'buddy-board-v3';
+const CACHE = 'buddy-board-v4';
 self.addEventListener('install', (e) => {
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll([
