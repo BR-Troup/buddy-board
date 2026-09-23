fa87c65942a6e09b9423f615e566ccfe300f0581
Buddy <buddy@grokbot.local>
2026-09-22 20:56:10 -0400

    Punchlist UX: tabs, email/done/follow-up/delete, notes; add overnight items

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'buddy-board-v1';
+const CACHE = 'buddy-board-v2';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './todos.json', './icon-180.png', './icon-192.png'];
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
